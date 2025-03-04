6416a50430be545f1f6e02f96b0cb7e3dc25c923
Alina web2025-3 <shablenkoo@gmail.com>
2025-03-04 16:56:46 +0200

    Додала Commander.js та підтримку аргументів командного рядка

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Головний файл програми працює!");
+const { Command } = require('commander');
+const program = new Command();
+
+program
+    .version('1.0.0')
+    .description('Програма для роботи з командним рядком')
+    .option('-n, --name <type>', 'Ваше ім’я', 'Користувач')
+    .option('-a, --age <number>', 'Ваш вік');
+
+program.parse(process.argv);
+
+const options = program.opts();
+console.log(`Привіт, ${options.name}!`);
+if (options.age) {
+    console.log(`Вам ${options.age} років.`);
+}
+
